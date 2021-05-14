a2ccf8aae4273eb075910da2989a9ecda76ee4ac
the-dev-eloper <imgnath28@gmail.com>
2021-05-14 12:22:47 +0530

    Data from Backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 import Language from '../components/Language'
 import data from '../data'
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 export default function HomeScreen() {
+
+    const [languages, setLanguages] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true);
+                const { data } = await axios.get('/api/languages');
+                setLoading(false);
+                setLanguages(data);
+            } catch (error) {
+                setError(error);
+                setLoading(false);
+            }
+        };
+        
+        fetchData();
+    }, [])
+
     return (
         <div>
 
-            <div class="row center">
+            {loading ? (
+                <LoadingBox></LoadingBox>
+            ) : error ? (
+                <MessageBox variant="danger">{error}</MessageBox>
+            ) : (
+                <div class="row center">
 
-                {data.languages.map((language) => (
-                    <Language key={language._id} language={language} />
-                ))}
-            </div>
+                    {languages.map((language) => (
+                        <Language key={language._id} language={language} />
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
