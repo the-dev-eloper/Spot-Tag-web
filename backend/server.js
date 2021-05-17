7f1b218c8be90382dc83b3e8a588ced93b851ce4
the-dev-eloper <imgnath28@gmail.com>
2021-05-17 17:51:47 +0530

    SignIn Backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose'
+import dotenv from 'dotenv';
 import languageRouter from './routers/languageRouter.js';
 import userRouter from './routers/userRouter.js';
 
+dotenv.config();
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/spottag-web', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
