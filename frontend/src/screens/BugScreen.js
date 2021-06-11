f5ad48bb1ef351cc2b69a1338632a2a07cd0d1c9
the-dev-eloper <imgnath28@gmail.com>
2021-06-11 20:02:59 +0530

    Load Bugs from Backend

--- frontend/src/screens/BugScreen.js
+++ frontend/src/screens/BugScreen.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios';
 import data from '../data';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 export default function BugScreen() {
-    return (
 
+    const [bugs, setBugs] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+
+            try {
+                setLoading(true);
+                const { data } = await Axios.get('/api/bugs');
+                setLoading(false);
+                setBugs(data);
+            } catch (error) {
+                setError(error);
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [])
+
+    return (
         <div class="grid-container">
 
             <h1>All Bugs</h1>
@@ -22,28 +46,36 @@ export default function BugScreen() {
                     </tr>
                 </thead>
 
-                <tbody>
-                    {
-                        data.bugs.map((bug) => (
-                            <tr key={bug._id}>
+                {
+                    loading ? (
+                        <LoadingBox></LoadingBox>
+                    ) : error ? (
+                        <MessageBox variant="danger">{error}</MessageBox>
+                    ) : (
+                        <tbody>
+                            {
+                                bugs.map((bug) => (
+                                    <tr key={bug._id}>
 
-                                <td>{bug.name}</td>
-                                <td>{bug.category}</td>
-                                <td>{bug.language}</td>
-                                <td>{bug.reason}</td>
-                                <td>{bug.testingTool}</td>
-                                <td>{bug.solution}</td>
+                                        <td>{bug.name}</td>
+                                        <td>{bug.category}</td>
+                                        <td>{bug.language}</td>
+                                        <td>{bug.reason}</td>
+                                        <td>{bug.testingTool}</td>
+                                        <td>{bug.solution}</td>
 
-                                <td>
-                                    <a href={bug.refLink}>
-                                        {bug.refLink}
-                                    </a>
-                                </td>
-                                <td>{bug.addedBy}</td>
-                            </tr>
-                        ))
-                    }
-                </tbody> 
+                                        <td>
+                                            <a href={bug.refLink}>
+                                                {bug.refLink}
+                                            </a>
+                                        </td>
+                                        <td>{bug.addedBy}</td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    )
+                }
             </table>
         </div> 
     );
