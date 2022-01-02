1e2edb56163c409f26d7914d2388ff1d9c1392aa
Gokulnath D <imgnath28@gmail.com>
2022-01-02 19:52:29 +0530

    #05 - Signup screen UI Frontend

--- frontend/src/screens/SignupScreen.js
+++ frontend/src/screens/SignupScreen.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+
 import { register } from '../actions/userActions';
+
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 
@@ -11,10 +12,12 @@ export default function SignupScreen(props) {
 
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
     const [mobile, setMobile] = useState(0);
     const [designation, setDesignation] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const [tnc, setTnc] = useState(false);
 
     const redirect = props.location.search ? props.location.search.split('=')[1] : '/';
 
@@ -29,9 +32,19 @@ export default function SignupScreen(props) {
         if (password !== confirmPassword) {
             alert('Password and confirm password are not match');
         } else {
-            dispatch(register(name, email, password));
+            dispatch(register(
+                name,
+                email,
+                password,
+                mobile,
+                designation
+            ));
         }
     };
+
+    const gotoLogin = () => {
+        props.history.push('/signin')
+    }
 
     useEffect(() => {
         if (userInfo) {
@@ -40,132 +53,128 @@ export default function SignupScreen(props) {
     }, [props.history, redirect, userInfo]);
 
     return (
-        <div className="body-login">
-            <div className="box-login">
+        <div className="body--signup">
 
-                <div className="col-div-6-login">
+            <section class="signupPage">
+                <div class="centered--signupBox">
 
-                    <img
-                        src={data.loginImage}
-                        alt="Login"
-                        title="Login"
-                    />
+                    <img src={data.loginImage} alt="Signup" title="Signup" />
+
+                    <section class="signup__content--wrap">
+
+                        <div class="signup__content--header">
+                            <h2>
+                                Hello World,<br />
+                                Welcome to Spot -
+                                <span>Tag!</span>
+                            </h2>
+                        </div>
+
+                        <div class="signup__content--form">
+                            <form onSubmit={submitHandler}>
+
+                                {loading && <LoadingBox></LoadingBox>}
+                                {error && <MessageBox variant="danger">{error}</MessageBox>}
+
+                                <div class="signup__form--inputSection">
+
+                                    <div class="signup__form--row">
+                                        <div class="signupForm--inputWrap">
+                                            <label htmlFor="name" className="formLabel">First Name*</label> <br/>
+                                            <input
+                                                type="text"
+                                                id="name"
+                                                placeholder="Name"
+                                                required
+                                                onChange={(e) => setName(e.target.value)}
+                                            />
+                                        </div>
+
+                                        <div class="signupForm--inputWrap">
+                                            <label htmlFor="email" className="formLabel">Email*</label> <br/>
+                                            <input
+                                                type="email"
+                                                id="email"
+                                                placeholder="Email"
+                                                required
+                                                onChange={(e) => setEmail(e.target.value)}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div class="signup__form--row">
+                                        <div class="signupForm--inputWrap">
+                                            <label htmlFor="mobile" className="formLabel">Mobile*</label> <br/>
+                                            <input
+                                                type="number"
+                                                id="mobile"
+                                                placeholder="Mobile"
+                                                required
+                                                onChange={(e) => setMobile(e.target.value)}
+                                            />
+                                        </div>
+
+                                        <div class="signupForm--inputWrap">
+                                            <label htmlFor="designation" className="formLabel">Designation*</label> <br/>
+                                            <input
+                                                type="text"
+                                                id="designation"
+                                                placeholder="Designation"
+                                                required
+                                                onChange={(e) => setDesignation(e.target.value)}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div class="signup__form--row">
+                                        <div class="signupForm--inputWrap">
+
+                                            <label htmlFor="password" className="formLabel">New Password*</label> <br/>
+                                            <input
+                                                type="password"
+                                                id="password"
+                                                placeholder="Password"
+                                                required
+                                                onChange={(e) => setPassword(e.target.value)}
+                                            />
+                                        </div>
+
+                                        <div class="signupForm--inputWrap">
+                                            <label htmlFor="confirmPassword" className="formLabel">Confirm Password*</label> <br/>
+                                            <input
+                                                type="password"
+                                                id="confirmPassword"
+                                                placeholder="Confirm Password"
+                                                required
+                                                onChange={(e) => setConfirmPassword(e.target.value)}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="signup__form--cbInputWrap">
+                                    <input
+                                        type="checkbox"
+                                        id="tncbox"
+                                        onChange={() => setTnc(!tnc)}
+                                        required
+                                    />
+                                    <span>I agree to all the Terms and Conditions</span>
+                                </div>
+
+                                <div class="signup__form--buttonSection">
+                                    <button type="submit" disabled={!tnc}>Sign up</button>
+                                </div>
+                            </form>
+                        </div>
+
+                        <div class="signup__form--newUser">
+                            <h5>Already have an account?</h5>
+                            <button onClick={() => gotoLogin()}>Log in</button>
+                        </div>
+                    </section>
                 </div>
-
-                <div className="col-div-6-login">
-
-                    <div className="headText-signup">
-                        <p>Hello World, Welcome to Spot-<span>Tag</span>..</p>
-                    </div>
-
-                    <div className="clearfix"></div>
-                    <br />
-
-                    <div className="formField-signup">
-                        <form className="form-signup" onSubmit={submitHandler}>
-
-                            {loading && <LoadingBox></LoadingBox>}
-                            {error && <MessageBox variant="danger">{error}</MessageBox>}
-
-                            <div className="col-div-3-signup">
-                                <label htmlFor="name" className="formLabel">First Name*</label> <br/>
-                                <input
-                                    type="text"
-                                    id="name"
-                                    placeholder="Name"
-                                    required
-                                    onChange={(e) => setName(e.target.value)}
-                                />
-                            </div>
-
-                            <div className="col-div-3-signup">
-                                <label htmlFor="email" className="formLabel">Email*</label> <br/>
-                                <input
-                                    type="email"
-                                    id="email"
-                                    placeholder="Email"
-                                    required
-                                    onChange={(e) => setEmail(e.target.value)}
-                                />
-                            </div>
-
-                            <div className="clearfix"></div>
-
-                            <div className="col-div-3-signup">
-                                <label htmlFor="mobile" className="formLabel">Mobile*</label> <br/>
-                                <input
-                                    type="number"
-                                    id="mobile"
-                                    placeholder="Mobile"
-                                    required
-                                    onChange={(e) => setMobile(e.target.value)}
-                                />
-                            </div>
-
-                            <div className="col-div-3-signup">
-                                <label htmlFor="designation" className="formLabel">Country*</label> <br/>
-                                <input
-                                    type="text"
-                                    id="designation"
-                                    placeholder="Designation"
-                                    required
-                                    onChange={(e) => setDesignation(e.target.value)}
-                                />
-                            </div>
-
-                            <div className="clearfix"></div>
-
-                            <div className="col-div-3-signup">
-                                <label htmlFor="password" className="formLabel">New Password*</label> <br/>
-                                <input
-                                    type="password"
-                                    id="password"
-                                    placeholder="Password"
-                                    required
-                                    onChange={(e) => setPassword(e.target.value)}
-                                />
-                            </div>
-
-                            <div className="col-div-3-signup">
-                                <label htmlFor="confirmPassword" className="formLabel">Confirm Password*</label> <br/>
-                                <input
-                                    type="password"
-                                    id="confirmPassword"
-                                    placeholder="Confirm Password"
-                                    required
-                                    onChange={(e) => setConfirmPassword(e.target.value)}
-                                />
-                            </div>
-
-                            <div className="clearfix"></div>
-                            <br />
-
-                            <label className="tnc-signup">
-                                <input
-                                    type="checkbox"
-                                    id="tnc"
-                                    required
-                                /> I agree to all the Terms and Conditions
-                            </label>
-
-                            <div className="clearfix"></div>
-                            <br />
-
-                            <div className="btn-section-signup">
-                                <button type="submit" className="signupbtn-signup">Sign up</button>
-                            </div>
-                        </form>
-                    </div>
-
-                    <div className="clearfix"></div>
-                    <br />
-
-                    <p className="accounted-signup">
-                        Already have an account?{' '}
-                        <Link to={`/login?redirect=${redirect}`}>Log in</Link>
-                    </p>
-                </div>
-            </div>
+            </section>
         </div>
     );
 }
