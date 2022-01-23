38dfff4e44d41bde19e6e5b50344e236c8b5dd0f
Gokulnath D <imgnath28@gmail.com>
2022-01-23 21:15:42 +0530

    environmental variables

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 
 const express = require('express');
+require('dotenv/config');
+
 const app = express();
+const api = process.env.API_URL;
 
 app.listen(3000, () => {
     console.log("Server is running at http://localhost:3000/");
-})
+});
