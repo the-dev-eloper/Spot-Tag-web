9899078277b90ffd67cdaeb013d687a2aa52d3bf
Gokulnath D <imgnath28@gmail.com>
2022-01-23 21:27:18 +0530

    additon of middleware

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,14 @@
 
 const express = require('express');
 require('dotenv/config');
+const morgan = require('morgan');
 
 const app = express();
 const api = process.env.API_URL;
+
+// Middleware
+app.use(express.json());
+app.use(morgan('tiny'));
 
 app.listen(3000, () => {
     console.log("Server is running at http://localhost:3000/");
