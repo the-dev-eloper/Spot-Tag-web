e6ed216ecfc527b83c6517620b1c72cb04715308
Gokulnath D <imgnath28@gmail.com>
2022-01-29 17:30:27 +0530

    Create API for Users

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -29,6 +29,10 @@ const userSchema = new mongoose.Schema({
         required: true,
         default: true,
     },
+    dateCreated: {
+        type: Date,
+        default: Date.now()
+    },
 });
 
 userSchema.virtual('id').get(function () {
--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -9,4 +9,26 @@ userRouter.get(`/`, async (req, res) => {
     res.send(userList);
 });
 
+userRouter.post(`/`, async (req, res) => {
+    const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        mobile: req.body.mobile,
+        country: req.body.country,
+        isAdmin: req.body.isAdmin,
+        passwordHash: req.body.passwordHash,
+    });
+
+    const createdUser = await newUser.save();
+
+    if(createdUser) {
+        res.status(201).json(createdUser)
+    } else {
+        res.status(500).json({
+            error: err,
+            success: false
+        })
+    }
+});
+
 module.exports = userRouter;
