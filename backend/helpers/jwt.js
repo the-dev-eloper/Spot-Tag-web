70a0b47c7160316178f333e7fb035fa334321867
Gokulnath D <imgnath28@gmail.com>
2022-01-30 22:25:13 +0530

    Excluding Routes from Authentication

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -1,12 +1,19 @@
 
 const expressJwt = require('express-jwt');
 
+const api = process.env.API_URL;
 const myCodeSecret = process.env.JWT_SECRET;
 
 function authJwt() {
     return expressJwt({
         secret: myCodeSecret,
         algorithms: ['HS256'],
+    }).unless({
+        path: [
+            `${api}/users/login`,
+            `${api}/users/register`,
+            {url: /\/api\/v1\/languages(.*)/ , methods: ['GET', 'OPTIONS'] },
+        ]
     });
 }
 
